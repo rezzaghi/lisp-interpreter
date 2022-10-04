13b1ff5b4c92c7c1af713d3f31a7a195f86b2568
rezzaghi <lbrezzaghi@gmail.com>
2022-10-03 21:57:32 -0300

    parse genesis

--- app/Main.hs
+++ app/Main.hs
@@ -4,11 +4,30 @@ module Main where
 
 
 import Prelude hiding (words)
-import Data.Text
+import Data.Text hiding (length, head)
+
+data Exp = Num Int | Literal Text | Tree [Exp] deriving (Eq, Ord, Read, Show)
 
 tokenize :: Text -> [Text]
 tokenize line = words $ replace ")" " ) " (replace "(" " ( " line)
 
+parse :: [Text] -> Exp
+parse (x:xs)
+  | length (x:xs) == 0 = error "no program"
+  | x == "(" = parseLeft xs
+  | otherwise = atomParse x
+
+parseLeft :: [Text] -> Exp 
+parseLeft (x:xs)
+  | x == ")" = error "unexpected"
+  | otherwise = Tree [Literal "left"] --Todo Parsing
+
+atomParse :: Text -> Exp
+atomParse atom
+  | atom == ")" = error "unexpected"
+  | otherwise = Literal atom --Todo atom type
+
 main :: IO ()
-main = print $ tokenize "(+ 1 1)"
+main = print $ parse $ tokenize "(begin (define r 10) (* pi (* r r)))" 
+
 
